ce335aae230d17f5a01d1530e53bf6cda4e01cc9
Drew Templeton <templetonag@appstate.edu>
2023-04-11 18:29:44 -0400

    extend make-playlist endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
-from authorization.auth import get_spotify_obj
-from authorization import top_songs
+# from src import auth
+from src import top_songs, auth
 
 app = Flask(__name__)
 
@@ -22,9 +22,15 @@ def top_tracks(timeframe):
     return response
 
 
-@app.route("/make-playlist")
-def make_playlist():
-    playlist = top_songs.recommended_playlist(25, 'medium_term')
-    response = jsonify(playlist)
+@app.route("/make-playlist/<type>/<timeframe>")
+def make_playlist(type, timeframe):
+    if (type == 'recommendation'):
+        playlist = top_songs.recommended_playlist(25, 'medium_term')
+
+    elif (type == 'top-tracks'):
+        playlist_url = top_songs.top_tracks_playlist(25, timeframe)
+    
+    response = {'url': playlist_url}
+    response = jsonify(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
